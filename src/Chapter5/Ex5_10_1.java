8ab82c3f6663b0d0ae46d83a36e15f3a60d3d024
HsKim4054 <88170117+HsKim4054@users.noreply.github.com>
2023-08-17 18:51:46 +0900

    ex

---
+++ src/Chapter5/Ex5_10_1.java
@@ -0,0 +1,42 @@
+package Chapter5;
+
+public class Ex5_10_1 {
+
+	public static void main(String[] args) {
+		char[] abcCode =
+			 { '`','~','!','@','#','$','%','^','&','*',
+			 '(',')','-','_','+','=','|','[',']','{',
+			 '}',';',':',',','.','/'};
+			 // 0 1 2 3 4 5 6 7 8 9
+		char[] numCode = {'q','w','e','r','t','y','u','i','o','p'};
+		String result = "`~!wer";
+		String src = "";
+		
+		for(int i =0;i<result.length();i++) {
+			char ch = result.charAt(i);
+			if(ch>= 'a'&& ch <= 'z') {
+				int num = 0;
+				for(int j=0;j<numCode.length;j++) {
+					if(ch == numCode[j])
+						break;
+					num++;
+				}
+				src += Integer.toString(num);
+			}
+			else {
+				int symbol  = 0;
+				for(int j=0;j<abcCode.length;j++) {
+					if(ch == abcCode[j])
+						break;
+					symbol++;
+				}
+				symbol += 97;
+				char ch2 = (char)symbol;
+				src += ch2;
+			}
+		}
+		System.out.println("result:"+result);
+		System.out.println("src:"+src);
+	}
+
+}
---
+++ src/Chapter5/Ex5_12.java
@@ -0,0 +1,28 @@
+package Chapter5;
+
+public class Ex5_12 {
+
+	public static void main(String[] args) {
+		// 3m 우물 바닥에 달팽이가 있다.
+		// 달팽이는 낮동안 55cm 올라올 수 있다. 하지만 날이 지면 잠을 자야 한다.
+		// 자는 동안 3cm 미끄러져 내려온다.
+		// 달팽이가 우물 밖으로 나오는데 몇일이 걸릴까?
+		// 반복문과 조건문을 이용하여 프로그램을 작성하시오.
+		int dis = 0;
+		int day = 0;
+		
+		while(true)
+		{
+			day += 1;
+			dis += 55;
+			
+			if(dis>=300)
+				break;
+			
+			dis -= 3;
+		}
+		System.out.println(day+"일 소요");
+		
+	}
+
+}
