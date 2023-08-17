c21cdbb50f274e13a493cc9abdc33f01abc6d6b9
HsKim4054 <88170117+HsKim4054@users.noreply.github.com>
2023-08-17 17:09:14 +0900

    modify

--- src/Chapter5/Ex5_10.java
+++ src/Chapter5/Ex5_10.java
@@ -13,16 +13,14 @@ public class Ex5_10 {
 			String result = "";
 			
 			for(int i=0; i < src.length();i++) {
-			char ch = src.charAt(i);
+				char ch = src.charAt(i);
 
-			if(ch >='a'&&ch<='z' )
-			{
-				result += abcCode[ch-'a'];
+				if(ch >='a'&&ch<='z' ) {
+					result += abcCode[ch-'a'];
+				}
+				else
+					result += numCode[ch-'0'];
 			}
-			else
-				result += numCode[ch-'0'];
-			}
-			
 			System.out.println("src:"+src);
 			System.out.println("result:"+result);
 	}
